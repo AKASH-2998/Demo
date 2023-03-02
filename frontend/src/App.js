ae21b412bafdbd977e0d77894cde5afa68777879
Akash <yoursakash29@gmail.com>
2023-03-02 22:03:39 +0530

    "Frontend Edit"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://localhost/goals:1010');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://localhost/goals:1010', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
